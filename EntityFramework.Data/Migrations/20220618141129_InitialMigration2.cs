9235e3e97cd0bad64a6348b19b24d836fac3e5d5
Abhimanyu Kumar <abhimanyu.kumar@kelltontech.org>
2022-06-19 18:19:49 +0530

    initial commit

---
+++ EntityFramework.Data/Migrations/20220618141129_InitialMigration2.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EntityFramework.Data.Migrations
+{
+    public partial class InitialMigration2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UniversityName",
+                table: "EmployeeEducations",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UniversityName",
+                table: "EmployeeEducations");
+        }
+    }
+}
